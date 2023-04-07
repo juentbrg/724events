a9074122b79b54d1fd842d067310dd46a4f7c841
juentbrg <julient.bourgeois@gmail.com>
2023-04-07 10:40:55 +0200

    Correction des balises dans index.html

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
+// import { useData } from "../../contexts/DataContext";
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -24,21 +25,27 @@ describe("When Form is created", () => {
       await screen.findByText("Message envoyé !");
     });
   });
-
 });
-
 
 describe("When a page is created", () => {
   it("a list of events is displayed", () => {
     // to implement
-  })
+  });
   it("a list a people is displayed", () => {
     // to implement
-  })
+  });
   it("a footer is displayed", () => {
     // to implement
-  })
+  });
   it("an event card, with the last event, is displayed", () => {
-    // to implement
-  })
+    // const { last } = useData();
+    // const eventCard = document.querySelector(".EventCard--small");
+    // const title = eventCard.querySelector(".EventCard__title");
+    // const date = eventCard.querySelector(".EventCard__month");
+    // const image = eventCard.querySelector('[data-testid="card-image-testid"]');
+    // expect(title).toBe(last.title);
+    // expect(date).toBe(last.date);
+    // expect(image.src).toBe(last.cover);
+    // expect(image.alt).toBe(last.title);
+  });
 });
